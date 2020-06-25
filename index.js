a52012c72f918d86f764658b31624f9bb98a6d88
beellz <belal20795@gmail.com>
2020-06-25 16:47:42 +0530

    adding the fs as json and making sure the append not able to make a new file

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ require('dotenv').config();
       await page.type("[name=username]", myid);
       await page.type('[type="password"]', mypass);
       await page.click("[type=submit]");
-      await page.waitForTimeout(4000);
+      await page.waitForTimeout(8000);
       await page.goto(`https://www.instagram.com/belalthecoolboy`);
-      await page.waitForTimeout(5000);
+      await page.waitForTimeout(8000);
     // await page.screenshot({path: `ea-${fullDay}.png`});
     await page.waitForSelector("img ", {
         visible: true,
@@ -33,13 +33,14 @@ require('dotenv').config();
      await page.screenshot({ path: `profile.png`});
      await page.waitForTimeout(2000);
      
-    const data = await page.evaluate(() => {
+    let data = await page.evaluate(() => {
         const images = document.querySelectorAll("img");
         const url = Array.from(images).map((v) => v.src);
         return url;
+     
     });
 
-    console.log(data);
+    console.log(typeof data);
     
     // fs.writeFile('newfile.txt', data ,  (err , data) => {
     //     if (err) throw err;
@@ -47,15 +48,19 @@ require('dotenv').config();
         
     // } );
 
+    let JSData = JSON.stringify(data)
+    let toJs = JSON.parse(JSData)
 
-
-    fs.appendFile('mynewfile1.txt', data , function (err) {
+    console.log(typeof toJs);
+    
+    fs.appendFile('mynewfile1.json', toJs , function (err) {
         if (err) throw err;
         console.log('Updated!');
       });
 
 
     await browser.close();
-    console.log(fullDay);
+    
 
 })();
+
